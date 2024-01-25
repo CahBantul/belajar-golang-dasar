4be60f1084a98a3ff9f96fbd5ea08322ae88ef6c
Fardan Nozami Ajitama <64827736+CahBantul@users.noreply.github.com>
2024-01-25 16:23:59 +0700

    initial commit

---
+++ variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string
+	
+	name := "fardan"
+	fmt.Println(name)
+	name = "Fardan Nozami"
+	fmt.Println(name)
+
+	var (
+		firstname = "Fardan"
+		middlename = "Nozami"
+		lastname = "Ajitama"
+	)
+
+	println(firstname)
+	println(middlename)
+	println(lastname)
+}
